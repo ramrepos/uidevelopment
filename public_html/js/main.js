9ed74680ea172958191ae5129251740dc180cd22
ramrepos <magnusram83@yahoo.com>
2018-01-23 17:32:28 +0530

    Using knockout for 2-way binding

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,10 +1,24 @@
-requirejs(["app/app","app/lib"], function(app, lib) {
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        app: '../app'
+    }
+});
+
+requirejs(["knockout-3.4.2"], function(ko) {
     //This function is called when app/app.js and app/lib.js is loaded.
     //If app.js calls define(), then this function is not fired until
     //app's dependencies have loaded, and the app argument will hold
     //the module value for "app/app".
     
-    var app = app;
-    var lib = lib;
+    function ViewModel(){
+        var self = this;
+        self.firstName = ko.observable('');
+        self.lastName = ko.observable('');
+        self.fullName = ko.pureComputed(function(){
+            return self.lastName() +  ', ' + self.firstName();
+        },self);  
+    }
     
+    ko.applyBindings(new ViewModel());
 });
