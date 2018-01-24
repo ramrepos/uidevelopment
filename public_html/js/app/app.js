28c4e84ec10da2bfc622be9d4aae5427b3161dd6
ramrepos <magnusram83@yahoo.com>
2018-01-24 10:12:40 +0530

    Unit1.1 Javascrtip file include

--- public_html/js/app/app.js
+++ public_html/js/app/app.js
@@ -1,12 +1,19 @@
- var app =
-         {
-             submitName : function() {
+function submitName() {
                  var fName = document.getElementById('firstName').value;
                 var lName = document.getElementById('lastName').value;
-                var fullName = lName + ',' + fName;
+                var fullName = '';
+                if (!isEmpty(fName) && !isEmpty(lName)){
+                    fullName = lName + ', ' + fName;
+                }                
                 document.getElementById('fullName').innerText = fullName;
              }
- };
+function isEmpty(val){
+    if (val && val.length > 0){
+        return false;
+    }
+    return true;
+}
+ 
 
             
         
